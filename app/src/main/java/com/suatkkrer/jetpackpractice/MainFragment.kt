28beae7ce5d72d0fd4bb87a5da7f1e1634e4eb86
suatkkrer <suat.kukrer@gmail.com>
2021-03-05 13:10:25 +0100

    background added

--- app/src/main/java/com/suatkkrer/jetpackpractice/MainFragment.kt
+++ app/src/main/java/com/suatkkrer/jetpackpractice/MainFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_main.*
 
 
 class MainFragment : Fragment() {
@@ -20,6 +22,19 @@ class MainFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main, container, false)
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        firstFragmentButton.setOnClickListener {
+
+            val action = MainFragmentDirections.actionMainFragmentToJokeFragment()
+            Navigation.findNavController(it).navigate(action)
+
+        }
+
     }
 
 }
